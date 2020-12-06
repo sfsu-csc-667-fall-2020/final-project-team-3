661797118b313f95762e63a72ffefae9095202ed
Ryan Hu <213edu@gmail.com>
2020-12-05 19:56:38 -0800

    changed server.js to more conventional app.js added couple npm pkg to set up codes for kafka, redis and mongoose added init code for kafka, redis and mongoose added .gitignore for untracking unessary files

--- back_end/app.js
+++ back_end/app.js
@@ -2,11 +2,31 @@
   main backend server file
 */
 
+// redis
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+// kafka
+const KafkaProducer = require('./kafka/KafkaProducer');
+const kafkaProducer = new KafkaProducer('images');
+
+// mongo
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://team3user:Pu5pTgsjoPmOsNLc@cluster0.qwxma.mongodb.net/team3?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// express
 const express = require('express');
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
 
-app.listen(port, () => console.log(`server started on ${port}`));
+app.use(express.urlencoded({extended: false}));
 
 // TODO
 app.post('/api/createListing', (req, res) => {
@@ -40,10 +60,12 @@ app.post('/api/makeInquiry', (req, res) => {
 
 // TODO
 app.post('/api/register', (req, res) => {
+  console.log(req.body);
   res.send('register');
 });
 
 // TODO
 app.post('/api/login', (req, res) => {
+  console.log(req.body);
   res.send('login');
 });
