573681cd6a5449dd24430b1386b449543e06f847
zfanc <55414743+zfanc@users.noreply.github.com>
2020-12-06 14:53:25 -0800

    trying to make login work

--- back_end/server/app.js
+++ back_end/server/app.js
@@ -5,93 +5,112 @@
 /*****************************
  *           redis           *
  *****************************/
-const redis = require('redis');
+const redis = require("redis");
 const redisClient = redis.createClient();
 
 /*****************************
  *           kafka           *
  *****************************/
-const KafkaProducer = require('../kafka/KafkaProducer');
-const kafkaProducer = new KafkaProducer('images');
+const KafkaProducer = require("../kafka/KafkaProducer");
+const kafkaProducer = new KafkaProducer("images");
 
 /*****************************
  *          passport         *
  *****************************/
-const passport = require('passport');
-require('../config/passport')(passport);
+const passport = require("passport");
+require("../config/passport")(passport);
 
 /*****************************
  *          mongoDB          *
  *****************************/
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://team3user:Pu5pTgsjoPmOsNLc@cluster0.qwxma.mongodb.net/team3?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+const mongoose = require("mongoose");
+
+const uri =
+  "mongodb+srv://team3user:Pu5pTgsjoPmOsNLc@cluster0.qwxma.mongodb.net/team3?retryWrites=true&w=majority";
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 /*****************************
  *          express          *
  *****************************/
-const express = require('express');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const flash = require("connect-flash");
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
 
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
 // Bodyparser
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // express session
-const session = require('express-session');
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}));
+const session = require("express-session");
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // EXPRESS ROUTES
 // requests gets routed to ../routes/ to keep app.js clean
-app.use('/api/users', require('../routes/users'));
-
+app.use("/", require("../routes/index.js"));
+app.use("/users", require("../routes/users"));
 
 /*****************************
  *          endpoints        *
  *****************************/
 // TODO
-app.post('/api/createListing', (req, res) => {
-  res.send('createListing');
+app.post("/api/createListing", (req, res) => {
+  res.send("createListing");
 });
 
 // TODO
-app.post('/api/editListing', (req, res) => {
-  res.send('editListing');
+app.post("/api/editListing", (req, res) => {
+  res.send("editListing");
 });
 
 // TODO
-app.get('/api/viewListings', (req, res) => {
-  res.send('viewListings');
+app.get("/api/viewListings", (req, res) => {
+  res.send("viewListings");
 });
 
 // TODO
-app.post('/api/deleteListing', (req, res) => {
-  res.send('deleteListing');
+app.post("/api/deleteListing", (req, res) => {
+  res.send("deleteListing");
 });
 
 // TODO
-app.get('/api/getInquiries', (req, res) => {
-  res.send('getInquiries');
+app.get("/api/getInquiries", (req, res) => {
+  res.send("getInquiries");
 });
 
 // TODO
-app.post('/api/makeInquiry', (req, res) => {
-  res.send('makeInquiry');
+app.post("/api/makeInquiry", (req, res) => {
+  res.send("makeInquiry");
 });
-
-
