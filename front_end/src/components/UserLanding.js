7cce13ea8df167825efdfa9d79783de3039d37ea
Matt Bolles <mattrbolles@gmail.com>
2020-12-09 18:00:32 -0800

    login stuff added, again

--- front_end/src/components/UserLanding.js
+++ front_end/src/components/UserLanding.js
@@ -7,22 +7,34 @@ import { userLogin } from "../redux/actions/loginActions";
 import loginReducer from "../redux/reducers/loginReducer";
 import Login from "./Login";
 import Register from "./Register";
+import { connect } from 'react-redux';
 // only display is user is not logged in - will give them option to log in or register
 
-const UserLanding = () => {
+const select = appState => ({
+    isLoggedIn: appState.loginReducer.isLoggedIn,
+    username: appState.loginReducer.username,
+    _id: appState.loginReducer._id,
+});
+
+
+const UserLanding = ({isLoggedIn}) => {
     return(
-        <div class = "grid-container">
-            <div class = "site-content" id = "site-content">
-                <div class = "user-landing">
-                    <h4>Login</h4>
-                    <Login />
-                    <h4>Need an account? Register here</h4>
-                    <Register />
+        <div>
+        {isLoggedIn && (
+            <div class = "grid-container">
+                <div class = "site-content" id = "site-content">
+                    <div class = "user-landing">
+                        <h4>Login</h4>
+                        <Login />
+                        <h4>Need an account? Register here</h4>
+                        <Register />
+                    </div>
                 </div>
             </div>
+        )}   
         </div>
     
     );
 }
 
-export default UserLanding;
+export default connect(select)(UserLanding);
